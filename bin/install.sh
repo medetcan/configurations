db5bf328e99958d0cbd1954fc78660a3a326a9af
medetcan <medetc.akkus@gmail.com>
2019-12-09 08:48:47 +0300

    installation script completed

---
+++ bin/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set +o errexit
+set -o nounset;
+
+PROJECT_ROOT=$(dirname $(dirname $(realpath "$0")));
+PACKAGES="${PROJECT_ROOT}/packages/bin/install.sh";
+NVIM="${PROJECT_ROOT}/nvim/bin/install.sh";
+VSCODE="${PROJECT_ROOT}/vscode/bin/install.sh";
+RUST="${PROJECT_ROOT}/rust/bin/install.sh";
+TEX="${PROJECT_ROOT}/tex/bin/install.sh";
+
+echo $PROJECT_ROOT;
--- nvim/bin/install_vim_plug.sh
+++ nvim/bin/install_vim_plug.sh
@@ -12,6 +12,7 @@ update_plugins() {
       echo "Installing and updating plugins...";
       if ! nvim --headless +PlugUpdate! +UpdateRemotePlugins +qa > /dev/null 2>&1; then
           echo "Unable to install and update plugins.";
+          exit 1;
       else
           echo "Vim plugins are installed and updated.";
       fi
@@ -21,11 +22,14 @@ if [ ! -e "$VIM_PLUG" ]; then
   echo "Vim plug is not installed."
   if ! curl -fLo "$VIM_PLUG" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null 2>&1; then
       echo "Unable to install Vim plug.";
+      exit 1;
   else
       echo "Vim plug is installed." 
       update_plugins;
+      exit 0;
   fi
 else
   echo "Vim Plug is already installed.";
   update_plugins;
+  exit 0;
 fi
